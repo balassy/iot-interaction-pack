78473d96accdcf11794197d1151f088a4a8a1963
György Balássy <balassy@users.noreply.github.com>
2015-11-12 11:49:37 +0100

    FIXED: Indentation and code formatting.

--- Wicip.Sample/ViewModels/SettingsPageViewModel.cs
+++ Wicip.Sample/ViewModels/SettingsPageViewModel.cs
@@ -8,23 +8,23 @@ namespace Wicip.Sample.ViewModels
 	public class SettingsPageViewModel : Template10.Mvvm.ViewModelBase
 	{
 		public AboutPartViewModel AboutPartViewModel { get; } = new AboutPartViewModel();
-}
+	}
 
 
 
 	public class AboutPartViewModel : Template10.Mvvm.ViewModelBase
-    {
-        [SuppressMessage("Microsoft.Performance", "CA1822:MarkMembersAsStatic", Justification = "View model")]
-        public Uri Logo => Package.Current.Logo;
+	{
+		[SuppressMessage( "Microsoft.Performance", "CA1822:MarkMembersAsStatic", Justification = "View model" )]
+		public Uri Logo => Package.Current.Logo;
 
-        [SuppressMessage("Microsoft.Performance", "CA1822:MarkMembersAsStatic", Justification = "View model")]
-        public string DisplayName => Package.Current.DisplayName;
+		[SuppressMessage( "Microsoft.Performance", "CA1822:MarkMembersAsStatic", Justification = "View model" )]
+		public string DisplayName => Package.Current.DisplayName;
 
-        [SuppressMessage("Microsoft.Performance", "CA1822:MarkMembersAsStatic", Justification = "View model")]
-        public string Publisher => Package.Current.PublisherDisplayName;
+		[SuppressMessage( "Microsoft.Performance", "CA1822:MarkMembersAsStatic", Justification = "View model" )]
+		public string Publisher => Package.Current.PublisherDisplayName;
 
-        [SuppressMessage("Microsoft.Performance", "CA1822:MarkMembersAsStatic", Justification = "View model")]
-        public string Version
+		[SuppressMessage( "Microsoft.Performance", "CA1822:MarkMembersAsStatic", Justification = "View model" )]
+		public string Version
 		{
 			get
 			{
@@ -33,8 +33,8 @@ namespace Wicip.Sample.ViewModels
 			}
 		}
 
-        [SuppressMessage("Microsoft.Performance", "CA1822:MarkMembersAsStatic", Justification = "View model")]
-        public string Homepage => @"https://github.com/balassy/wicip";
+		[SuppressMessage( "Microsoft.Performance", "CA1822:MarkMembersAsStatic", Justification = "View model" )]
+		public string Homepage => @"https://github.com/balassy/wicip";
 	}
 
 }
