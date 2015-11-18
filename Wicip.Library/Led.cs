ee5346317b1f0e4a0db825a782eed5f967f99e0e
György Balássy <balassy@users.noreply.github.com>
2015-11-18 10:03:48 +0100

    ADDED: Basic Led class.

---
+++ Wicip.Library/Led.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Windows.Devices.Gpio;
+
+namespace Wicip
+{
+	public class Led
+	{
+		GpioPin pin;
+
+		public Led( int pinId )
+		{
+			GpioController controller = GpioController.GetDefault();
+			this.pin = controller.OpenPin( pinId );
+			this.pin.Write( GpioPinValue.High );
+			this.pin.SetDriveMode( GpioPinDriveMode.Output );
+		}
+
+
+		public void TurnOn()
+		{
+			this.pin.Write( GpioPinValue.Low );
+		}
+
+
+		public void TurnOff()
+		{
+			this.pin.Write( GpioPinValue.High );
+		}
+	}
+}
