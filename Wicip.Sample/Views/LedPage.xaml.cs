bf00256f0e2812f9c8c787534b8a5ecd04083438
György Balássy <balassy@users.noreply.github.com>
2015-11-19 02:03:32 +0100

    ADDED: Data binded view-model to the LED page.

--- Wicip.Sample/Views/LedPage.xaml.cs
+++ Wicip.Sample/Views/LedPage.xaml.cs
@@ -1,30 +1,57 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace Wicip.Sample.Views
 {
-	/// <summary>
-	/// An empty page that can be used on its own or navigated to within a Frame.
-	/// </summary>
-	public sealed partial class LedPage : Page
+	internal sealed partial class LedPage : Page, IDisposable	
 	{
+		private Led led;
+
+
 		public LedPage()
 		{
 			this.InitializeComponent();
 		}
+
+
+		public void Dispose()
+		{
+			if( this.led != null )
+			{
+				this.led.Dispose();
+			}
+		}
+
+
+		protected override void OnNavigatingFrom( NavigatingCancelEventArgs e )
+		{
+			base.OnNavigatingFrom( e );
+
+			this.Dispose();
+		}
+
+
+		private void btnStartControl_Click( object sender, RoutedEventArgs e )
+		{
+			this.led = new Led( this.viewModel.PinNumber );
+
+			this.txbIsOnPrompt.Visibility = Visibility.Visible;
+			this.tsIsOn.Visibility = Visibility.Visible;
+		}
+
+		private void tsIsOn_Toggled( object sender, RoutedEventArgs e )
+		{
+			if( this.viewModel.IsOn )
+			{
+				this.led.TurnOn();
+			}
+			else
+			{
+				this.led.TurnOff();
+			}
+		}
+
 	}
 }
